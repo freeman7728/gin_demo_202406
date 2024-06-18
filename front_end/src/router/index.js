06f826f7d2d691246cabb987a4d17ba019b022c7
iGxy0929 <1530122381@qq.com>
2024-06-18 13:10:39 +0800

    主页面

--- front_end/src/router/index.js
+++ front_end/src/router/index.js
@@ -1,9 +1,17 @@
 import { createRouter, createWebHistory } from "vue-router";
 import login from "@/pages/logins/login.vue";
+import index_admin from "@/pages/indexs/index_admin.vue";
+import index_ordinary from "@/pages/indexs/index_ordinary.vue";
+import exception_window from "@/pages/childpages/exception_window.vue";
+import user_info from "@/pages/childpages/user_info.vue";
 
 const routes = [
   { path: "/", redirect: "/login" },
+  { path: "/childpages/user_info", name: "userInfo", component: user_info},
   { path: "/login", component: login},
+  { path: "/index_admin", component: index_admin},
+  { path: "/childpages/exception_window", component: exception_window},
+  { path: "/index_ordinary", component: index_ordinary},
 ];
 
 const router = createRouter({
