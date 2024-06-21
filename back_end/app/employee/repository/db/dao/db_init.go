fa671c7a4b22f79b345668588f06fd085da53be1
freeman7728 <1357880399@qq.com>
2024-06-21 11:28:43 +0800

    test

--- back_end/app/employee/repository/db/dao/db_init.go
+++ back_end/app/employee/repository/db/dao/db_init.go
@@ -24,7 +24,6 @@ func InitDB() {
 	password := config.DbPassWord
 	charset := config.Charset
 	dsn := strings.Join([]string{username, ":", password, "@tcp(", host, ":", port, ")/", database, "?charset=" + charset + "&parseTime=true"}, "")
-	fmt.Println(dsn)
 	err := Database(dsn)
 	if err != nil {
 		fmt.Println(err)
