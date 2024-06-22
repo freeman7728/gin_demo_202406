11748d54045cd39b8b5e06766996d8a1aac19246
freeman7728 <1357880399@qq.com>
2024-06-22 14:06:03 +0800

    插入产品时检查供应商是否存在并且返回信息

--- back_end/v2/services/product_service.go
+++ back_end/v2/services/product_service.go
@@ -17,6 +17,10 @@ func InsertProductService(list *dto.ProductList) (err error, resp dto.Response) 
 		if res.Error != nil {
 			resp.Code = http.StatusBadRequest
 			item.Msg = res.Error.Error()
+			var mysqlErr *mysql.MySQLError
+			if errors.As(res.Error, &mysqlErr) && mysqlErr.Number == 1452 {
+				resp.Message = "产品插入失败，检查供应商是否存在"
+			}
 			l.FailList = append(l.FailList, item)
 		} else {
 			l.SuccessList = append(l.SuccessList, item)
